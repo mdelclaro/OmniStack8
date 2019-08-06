fb30f2bccde8fb40965912166987836ca843f63e
Matheus Del Claro <matheusdelclaro@gmail.com>
2019-08-06 17:35:37 -0300

    dislike added

--- src/routes/Dev.js
+++ src/routes/Dev.js
@@ -3,6 +3,7 @@ const DevController = require('../controllers/Dev');
 
 const routes = express.Router();
 
+routes.get('/', DevController.index);
 routes.post('/', DevController.store);
 
 module.exports = routes;
