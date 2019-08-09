d0af636c0fce33f371934d1efda59ce49b3b6bef
Matheus Del Claro <matheusdelclaro@gmail.com>
2019-08-08 21:36:34 -0300

    api set up on login; main design done

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
+
+import api from '../services/api';
 
 import logo from '../assets/logo.svg';
 
-function Login() {
+function Login({ history }) {
+  const [username, setUsername] = useState('');
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const response = await api.post('/devs', {
+      username
+    });
+
+    const { _id } = response.data;
+
+    history.push(`/dev/${_id}`);
+  }
+
   return (
     <div className="login-container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <img src={logo} alt="Tindev" />
-        <input placeholder="Enter your Github username" />
+        <input
+          placeholder="Enter your Github username"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
         <button type="submit">Submit</button>
       </form>
     </div>
--- front-end/src/pages/Main.js
+++ front-end/src/pages/Main.js
@@ -1,7 +1,102 @@
 import React from 'react';
+import './Main.css';
 
-function Main() {
-  return <div />;
+import logo from '../assets/logo.svg';
+import dislike from '../assets/dislike.svg';
+import like from '../assets/like.svg';
+
+function Main({ match }) {
+  return (
+    <div className="main-container">
+      <img src={logo} alt="Tindev" />
+      <ul>
+        <li>
+          <img src={logo} alt="" />
+          <footer>
+            <strong>Matheus Del Claro</strong>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Assumenda rerum quae inventore qui voluptatibus expedita eveniet,
+              dicta reprehenderit voluptatem error aut laborum id a perferendis
+              ipsum nulla odio quas porro.
+            </p>
+          </footer>
+
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="Dislike" />
+            </button>
+            <button type="button">
+              <img src={like} alt="Like" />
+            </button>
+          </div>
+        </li>
+        <li>
+          <img src={logo} alt="" />
+          <footer>
+            <strong>Matheus Del Claro</strong>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Assumenda rerum quae inventore qui voluptatibus expedita eveniet,
+              dicta reprehenderit voluptatem error aut laborum id a perferendis
+              ipsum nulla odio quas porro.
+            </p>
+          </footer>
+
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="Dislike" />
+            </button>
+            <button type="button">
+              <img src={like} alt="Like" />
+            </button>
+          </div>
+        </li>
+        <li>
+          <img src={logo} alt="" />
+          <footer>
+            <strong>Matheus Del Claro</strong>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Assumenda rerum quae inventore qui voluptatibus expedita eveniet,
+              dicta reprehenderit voluptatem error aut laborum id a perferendis
+              ipsum nulla odio quas porro.
+            </p>
+          </footer>
+
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="Dislike" />
+            </button>
+            <button type="button">
+              <img src={like} alt="Like" />
+            </button>
+          </div>
+        </li>
+        <li>
+          <img src={logo} alt="" />
+          <footer>
+            <strong>Matheus Del Claro</strong>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Assumenda rerum quae inventore qui voluptatibus expedita eveniet,
+              dicta reprehenderit voluptatem error aut laborum id a perferendis
+              ipsum nulla odio quas porro.
+            </p>
+          </footer>
+
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="Dislike" />
+            </button>
+            <button type="button">
+              <img src={like} alt="Like" />
+            </button>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Main;
